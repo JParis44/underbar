225b3ef84ed21014bbb46d0d813b30ec4f0ff3d7
Paris Caldwell <j.paris.caldwell@gmail.com>
2015-05-11 17:33:03 -0500

    Completed _.map

--- src/underbar.js
+++ src/underbar.js
@@ -104,6 +104,11 @@
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
+    var results = [];
+      for (var i = 0; i < collection.length; i++) {
+        results.push(iterator(collection[i], i, collection));
+      }
+    return results;
   };
 
   /*
@@ -145,6 +150,15 @@
   //   }); // should be 5, regardless of the iterator function passed in
   //          No accumulator is given so the first element is used.
   _.reduce = function(collection, iterator, accumulator) {
+    if (Array.isArray(collection)) {
+      for (var i = 0; i < collection.length; i++) {
+        iterator(collection[i], i, collection);
+      }
+    } else {
+      for (var key in collection) {
+        iterator(collection[key], key, collection);
+      }
+    }
   };
 
   // Determine if the array or object contains a given value (using `===`).
